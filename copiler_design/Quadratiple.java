38e3e808c556b8c91d469fc21a4afc3b3a9e4402
navneetthakor <tnavneet8628@gmail.com>
2024-10-17 18:10:46 +0530

    Quadratiple generator for Compiler design

---
+++ copiler_design/Quadratiple.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Stack;
+
+class QuadratipleEntry{
+    final String opt;
+    final String arg1;
+    final String arg2;
+    final String temp_result;
+
+    QuadratipleEntry(String opt, String arg1, String arg2, String t_r){
+        this.opt = opt;
+        this.arg1 = arg1;
+        this.arg2 = arg2;
+        this.temp_result = t_r;
+    }
+
+    @Override
+    public String toString(){
+        return (this.opt + "\t" + this.arg1 + "\t" + this.arg2 + "\t" + this.temp_result);
+    }
+}
+
+class QuadratipleGenerator {
+    Stack<String> st;
+    int count;
+
+    QuadratipleGenerator(){
+        this.st = new Stack<>();
+        this.count = 0;
+    }
+
+    private String symolGenerator(){
+        return ("t" + this.count);
+    }
+
+    public ArrayList<QuadratipleEntry> solve(String postfix){
+
+        // clear stack first 
+        st.clear();
+
+        // required data structures 
+        ArrayList<QuadratipleEntry> ans = new ArrayList<>();
+        HashSet<Character> lookup = new HashSet<>();
+        lookup.add('+');
+        lookup.add('-');
+        lookup.add('*');
+        lookup.add('/');
+        lookup.add('^');
+
+        for(Character ch : postfix.toCharArray()){
+            if(!lookup.contains(ch)){
+                st.add(Character.toString(ch));
+                continue;
+            }
+
+            String arg2 = st.pop();
+            String arg1 = st.pop();
+            String temp_result = this.symolGenerator();
+
+            // adding in data structure 
+            ans.add(new QuadratipleEntry(Character.toString(ch), arg1, arg2, temp_result));
+            st.push(temp_result);
+            
+        }
+
+        return ans;
+    }
+    
+}
+
+public class Quadratiple {
+    public static void main(String[] args) {
+        QuadratipleGenerator parser = new QuadratipleGenerator();
+        String postfix = "ab+c*";
+
+        ArrayList<QuadratipleEntry> ans = parser.solve(postfix);
+
+        System.out.println("opt\targ1\targ2\ttemp_result");
+        for(QuadratipleEntry t : ans){
+            System.out.println(t);
+        }
+    }
+}
